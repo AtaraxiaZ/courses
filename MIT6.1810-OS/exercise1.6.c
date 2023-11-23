a28e0476df28b48dcccf198b22d28b0a1616123e
Zhe Li <904016301@qq.com>
2023-11-23 15:24:30 +0800

    first commit

---
+++ MIT6.1810-OS/exercise1.6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+void main() {
+  // p1: child write byte to father
+  // p2: father write byte to child
+  int p1[2];
+  int p2[2];
+  char *buf1;
+  char *buf2;
+
+  pipe(p1);
+  pipe(p2);
+  if (fork() == 0) {
+    write(p1[1], "c", 1);
+    while (1) {
+      if (read(p2[0], buf1, 1)) {
+        write(p1[1], buf1, 1);
+        printf("%c", buf1[0]);
+      }
+    }
+  } else {
+    while (1) {
+      if (read(p1[0], buf2, 1)) {
+        write(p2[1], buf2, 1);
+        printf("%c", buf2[0]);
+      }
+    }
+  }
+}
